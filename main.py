0a229e7399b102721fa62fe003f2dbf6914f1830
Jeff Shively <jshively@paloaltonetworks.com>
2026-07-30 09:51:58 -0400

    Writing all three sections to a file

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ AUTH_HEADERS = {
 }
 
 POLICY_ENDPOINTS = {
-    "security": "/security",
-    "access_and_data": "/access-and-data",
-    "customization": "/customization",
+    "security": "security",
+    "access_and_data": "access-and-data",
+    "customization": "customization",
 }
 
 load_dotenv()
@@ -28,7 +28,7 @@ TSG_ID = os.environ.get("TSG_ID")
 CLIENT_ID = os.environ.get("CLIENT_ID")
 SECRET_ID = os.environ.get("SECRET_ID")
 
-OUTPUT_DIR = 'output'
+OUTPUT_DIR = "output"
 
 
 def create_token():
@@ -43,32 +43,38 @@ def create_token():
     HEADERS.update({"Authorization": f'Bearer {token["access_token"]}'})
 
 
-def get_all_pb_policy():
-    url = f"{BASE_API_URL}/seb-api/v1/policy/access-and-data"
-    response = requests.request(
-        method="GET",
-        url=url,
-        headers=HEADERS
-    )
-    return response.json()
+def get_full_policy(policy_endpoint):
+    _ = []
+    url = f"{BASE_API_URL}/{policy_endpoint}"
+    response = requests.get(url, headers=HEADERS).json()
+    _.extend(response.get("data", []))
+    return _
 
 
-def get_single_pb_policy(id):
-    url = f"{BASE_API_URL}/seb-api/v1/policy/access-and-data/rules/{id}"
-    response = requests.request(
-        method="GET",
-        url=url,
-        headers=HEADERS
-    )
-    return response.json()
+def get_single_policy(policy_endpoint, id):
+    url = f"{BASE_API_URL}/{policy_endpoint}/rules/{id}"
+    response = requests.get(url, headers=HEADERS).json()
+    return response
+
 
 if __name__ == "__main__":
     create_token()
-    full_policy = []
-    policies = get_all_pb_policy()
-    ids = [item['id'] for item in policies.get('data', [])]
-    for id in ids:
-        response = get_single_pb_policy(id)
-        full_policy.append(response)
-    with open(f"{OUTPUT_DIR}/full_policy.json", "w") as f:
-        json.dump(full_policy, f, indent=4)
+    all_policy_ids = {}
+    all_policy_rules = {}
+    for policy_endpoint in POLICY_ENDPOINTS.values():
+        _ = []
+        _.extend(get_full_policy(policy_endpoint))
+        all_policy_ids[policy_endpoint] = _
+    ids_by_section = {
+        key: [rule["id"] for rule in rules if "id" in rule]
+        for key, rules in all_policy_ids.items()
+    }
+
+    for section, ids in ids_by_section.items():
+        all_policy_rules[section] = []
+        for id in ids:
+            print(f"Getting policy for section: {section}, id: {id}")
+            response = get_single_policy(section, id)
+            all_policy_rules[section].append(response)
+    with open(f"{OUTPUT_DIR}/all_policy_rules.json", "w", encoding="utf-8") as f:
+        json.dump(all_policy_rules, f, indent=2)
