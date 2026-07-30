95c20aa025f678214bc1a2b2a38536d7bb03fcb5
Jeff Shively <jshively@paloaltonetworks.com>
2026-07-30 11:15:20 -0400

    Adding in argparse for print and export. Import has a placeholder message.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import argparse
 import os
 import json
-import requests
+import sys
 
+import requests
 from dotenv import load_dotenv
 
 BASE_AUTH_URL = "https://auth.apps.paloaltonetworks.com/auth/v1/oauth2/access_token"
@@ -31,6 +33,19 @@ SECRET_ID = os.environ.get("SECRET_ID")
 OUTPUT_DIR = "output"
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Prisma Browser Policy Management Tool"
+    )
+    subparsers = parser.add_subparsers(dest="mode", required=True, help="Select mode")
+    subparsers.add_parser(
+        "export", description="Copy policy to a JSON file for later use or analysis"
+    )
+    subparsers.add_parser("print", description="Print policy to console")
+    subparsers.add_parser("import", description="Import policy from tenant to tenant")
+    return parser.parse_args()
+
+
 def create_token():
     auth_url = f"{BASE_AUTH_URL}?grant_type=client_credentials&scope:tsg_id:{TSG_ID}"
 
@@ -58,6 +73,12 @@ def get_single_policy(policy_endpoint, id):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print(
+            "Please provide either 'export', 'print', or 'import' when running the script.\nFor example: python main.py export"
+        )
+        sys.exit(1)
+    args = parse_args()
     create_token()
     all_policy_ids = {}
     all_policy_rules = {}
@@ -76,5 +97,11 @@ if __name__ == "__main__":
             print(f"Getting policy for section: {section}, id: {id}")
             response = get_single_policy(section, id)
             all_policy_rules[section].append(response)
-    with open(f"{OUTPUT_DIR}/all_policy_rules.json", "w", encoding="utf-8") as f:
-        json.dump(all_policy_rules, f, indent=2)
+
+    if args.mode == "export":
+        with open(f"{OUTPUT_DIR}/all_policy_rules.json", "w", encoding="utf-8") as f:
+            json.dump(all_policy_rules, f, indent=2)
+    elif args.mode == "print":
+        print(json.dumps(all_policy_rules, indent=2))
+    elif args.mode == "import":
+        print("Importing policy from tenant to tenant is not yet implemented.")
